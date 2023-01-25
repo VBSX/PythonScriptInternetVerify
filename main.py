194067536a75637bcd0b9847313793e8268e4d4f
Vitor <84788940+VBSX@users.noreply.github.com>
2023-01-25 12:05:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ class Main():
 
     def configurar_navegador(self):
  
-        self.internet_tester.matar_processo_navegador()
+ 
+        try:
+            self.internet_tester.matar_processo_navegador()
 
-        browser = BrowserController()
-        self.abrir_navegador(browser) 
-        try:    
+            browser = BrowserController()
+            self.abrir_navegador(browser)
             browser.configurar_som()
         except:
             pass
@@ -31,12 +32,12 @@ class Main():
     
     def loop_verifier(self):
         
-        
-        try:
-            t1 = threading.Thread(target=self.configurar_navegador)
-            t1.start()
-        except:
-            pass
+        if self.internet_tester.check_internet():
+            try:
+                t1 = threading.Thread(target=self.configurar_navegador)
+                t1.start()
+            except:
+                pass
         loop = True
         loop2 = True
         while loop:    
@@ -48,16 +49,25 @@ class Main():
                     time.sleep(5)
 
                 print('Internet OK!')
-               
-                self.internet_tester.matar_processo_navegador()
-                
-                browser = BrowserController()
-                
-                self.abrir_navegador(browser) 
-                
-                browser.configurar_som()
-                
-                              
+                try:
+                    self.internet_tester.matar_processo_navegador()
+                    
+                    browser = BrowserController()
+                    
+                    self.abrir_navegador(browser) 
+                    
+                    browser.configurar_som()
+                except:
+                    final_funcao= False
+                    while not final_funcao:
+                        self.internet_tester.matar_processo_navegador()
+                        
+                        browser = BrowserController()
+                        
+                        self.abrir_navegador(browser) 
+                        
+                        browser.configurar_som()
+                        final_funcao = True
            
 
             else:
